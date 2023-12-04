99b022334e70577cb4260d0c18adac8dca02c096
mickfay <mick.fay@hotmail.com>
2023-12-04 11:48:00 +0000

    Day 2 Task 2 Complete

--- src/2_cube_game.js
+++ src/2_cube_game.js
@@ -1,5 +1,6 @@
 function cubeGame(inputStr, expRed, expGreen, expBlue) {
   let validGames = 0;
+  let totalPower = 0;
   const gamesArr = inputStr.split("\n");
   console.log(gamesArr)
   const gameObjects = gamesArr.map((game, index) => {
@@ -12,16 +13,18 @@ function cubeGame(inputStr, expRed, expGreen, expBlue) {
     const maxRed = Math.max(...red);
     const maxGreen = Math.max(...green);
     const maxBlue = Math.max(...blue);
+    const power = maxGreen * maxRed * maxBlue
 
-    return { id, maxRed, maxBlue, maxGreen };
+    return { id, maxRed, maxBlue, maxGreen, power };
   });
 console.log(gameObjects)
   gameObjects.forEach((game) => {
     if (game.maxRed <= expRed && game.maxBlue <= expBlue && game.maxGreen <= expGreen){
         validGames += game.id
     }
+    totalPower += game.power
   })
-  return validGames
+  return {validGames, totalPower}
 }
 
 module.exports = { cubeGame };
